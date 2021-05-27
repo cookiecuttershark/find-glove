60d22a7861cb1d6075ab29cb3dd54f7fc9070245
cookiecuttershark <mattea42@gmail.com>
2021-05-27 09:17:50 -0400

    add router

---
+++ src/contact/contact.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Contact = () => {
+  return <p>contact form here</p>;
+};
+
+export default Contact;
--- src/page_container.js
+++ src/page_container.js
@@ -1,13 +1,26 @@
 import React from "react";
 import Menu from "./menu/menu";
-import About from './about/about'
+import About from "./about/about";
+import Contact from "./contact/contact";
+import Home from "./home/home";
+import { BrowserRouter, Route } from "react-router-dom";
 
 const PageContainer = () => {
   return (
     <>
       <h1>Find Glove</h1>
       <Menu />
-      <About/>
+      <BrowserRouter>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </BrowserRouter>
     </>
   );
 };
